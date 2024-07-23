bd00abdf257456c8dbe64302381ecfa01107e8b4
srinidhisg88 <srinidhisg1@gmail.com>
2024-07-23 22:01:01 +0530

    added a file

---
+++ searchin2dmatrix.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        for i in matrix:
+            if target in i:
+                return True
+            else:
+                continue
+        return False
