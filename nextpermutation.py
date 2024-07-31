62c855ca509067a798508d346b525ef6d8d492d1
srinidhisg88 <srinidhisg1@gmail.com>
2024-07-31 22:05:37 +0530

    updated

--- nextpermutation.py
+++ nextpermutation.py
@@ -2,7 +2,7 @@ class Solution:
     def nextPermutation(self,nums):
         # find the break point where there is a sudden dip
         ind=-1
-        for i in range(len(nums)-2,-1):
+        for i in range(len(nums)-2,-1,-1):
             if nums[i]<nums[i+1]:
                 ind=i
                 break
@@ -10,15 +10,13 @@ class Solution:
             nums.reverse()
             return nums
         #find a number which closely greater than nums[i]
-        for i in range(len(nums)-2,ind-1):
-            if(nums[i]>nums[ind]):
-                temp=nums[ind]
-                nums[ind]=nums[i]
-                nums[i]=temp
+        for i in range(len(nums)-1,ind,-1):
+            if nums[i]>nums[ind]:
+                nums[i],nums[ind]=nums[ind],nums[i]
                 break
         #sort the remaining part after break point to get the lowest arrangement
         nums[ind+1:]=reversed(nums[ind+1:])
         return nums
 soln=Solution()
-arr=[3,2,1]
+arr=[1,2,3]
 print(soln.nextPermutation(arr))
