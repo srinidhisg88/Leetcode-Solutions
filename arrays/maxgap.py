74a57d911025ef735269357b0d0f925ccaa725da
srinidhisg88 <srinidhisg1@gmail.com>
2024-09-10 21:16:37 +0530

    added a new soln of array

---
+++ arrays/maxgap.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maximumGap(self, nums: List[int]) -> int:
+        nums.sort()
+        n=len(nums)
+        if n<2:
+            return 0
+        if n==2:
+            return abs(nums[0]-nums[1])
+        hashmap=defaultdict(int)
+        maxdiff=float('-inf')
+        for i in range(n-1):
+            hashmap[i]=abs(nums[i]-nums[i+1])
+            maxdiff=max(hashmap[i],maxdiff)
+        return int(maxdiff)
