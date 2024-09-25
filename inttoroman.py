19685ac1070487166d43992a5d29cfb7eb15cc1f
srinidhisg88 <srinidhisg1@gmail.com>
2024-09-25 11:44:16 +0530

    added new soln

---
+++ inttoroman.py
@@ -0,0 +1,10 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        res=''
+        syslist=[['I',1],['IV',4],['V',5],['IX',9],['X',10],['XL',40],['L',50],['XC',90],['C',100],['CD',400],['D',500],['CM',900],['M',1000]]
+        for sys,val in reversed(syslist):
+            if num//val:
+                count=num//val
+                res+=sys*count
+                num=num%val
+        return res
